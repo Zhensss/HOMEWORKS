253221d7b5929fdfd6d5cdc7a1c2b1b56ae7666d
Zhensss <149566540+Zhensss@users.noreply.github.com>
2024-06-26 19:40:53 +0400

    6.04 to les 39

---
+++ homework6-04.py
@@ -0,0 +1,32 @@
+# 1
+# def division(x, y):
+#     try:
+#         print(x/y)
+#     except ZeroDivisionError:
+#         print('error')
+        
+# division(4, 0)
+# division(7, 8)
+        
+# 2
+# def validate(num):
+#     try:
+#         num2 = float(num)
+#         if num2:
+#             print('Ok')
+#     except ValueError:
+#         print('ERROR')
+    
+# validate(12)
+# validate('ls')
+        
+# 3
+# def validate33(number1):
+#     try:
+#         number12 = float(number1)
+#         print('OK')
+#     except ValueError:
+#         print('ERROR')
+        
+# validate33(12)
+# validate33('djdd')
